cbf70b053a78d8fa77ba447a5b332c9c4436263d
ShadowGamerXY <shadowgamerxy@gmail.com>
2016-07-07 19:12:05 +0200

    1.10.2

---
+++ src/main/java/SirShadow/Hemomancy/Hemomancy.java
@@ -0,0 +1,11 @@
+package SirShadow.Hemomancy;
+
+import net.minecraftforge.fml.common.Mod;
+
+/**
+ * Created by SirShadow on 7. 07. 2016.
+ */
+@Mod(modid = "hemomancy", name = "Hemomancy",version = "0.2")
+public class Hemomancy
+{
+}
