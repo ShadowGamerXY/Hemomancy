92e43a3534ef1d7cb91c7f2bc4c4915e4af46d4a
ShadowGamerXY <shadowgamerxy@gmail.com>
2016-07-07 19:26:15 +0200

    1.10.2 - initial setup

--- src/main/java/SirShadow/Hemomancy/Hemomancy.java
+++ src/main/java/SirShadow/Hemomancy/Hemomancy.java
@@ -6,8 +6,7 @@ import net.minecraftforge.fml.common.Mod;
  * Created by SirShadow on 7. 07. 2016.
  */
 @Mod(modid = "hemomancy", name = "Hemomancy",version = "0.2")
-public class Hemomancy
-{
+public class Hemomancy {
     @Mod.Instance("hemomancy")
     public static Hemomancy instance;
 }
